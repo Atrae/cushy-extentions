20069847eb6111c8f1ad8b00b0ccf476a8350105
Yuki Moriyama <yuki.moriyaman@gmail.com>
2014-10-27 21:45:14 +0900

    autofill機能追加

--- chrome-extention/content_frame.js
+++ chrome-extention/content_frame.js
@@ -1,9 +1,12 @@
 var saveDialog = new saveDialog();
 var localData = new localData();
 
+//chrome.storage.local.clear(function(){ });
+
 chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
   var tempData = msg.tempData;
   if(msg.action === "openDialogBox"){
+    // open dialog box
     var button = '<input type="submit" class="cushy-ext-submit-js" style="width: 70px; height: 25px; margin: 0;" value="登録する">';
     saveDialog.button = button;
     saveDialog.message = "登録しますか？"
@@ -12,20 +15,22 @@ chrome.extension.onMessage.addListener(function(msg, sender, sendResponse) {
       saveDialog.submit(msg.tempData);
     });
   }else if(msg.action === "noLoginNotification"){
+    // prompt login
     saveDialog.message = "現在未ログインのようです。ログインしていただかないとアカウントは登録されません。";
     saveDialog.button = '';
     saveDialog.insert();
   }else if(msg.action === "fillAccount" ){
+    // fill account
     var loginElementName = 'input[name="'+ msg.accountData.loginElementName + '"]';
     var passwordElementName = 'input[name="'+ msg.accountData.passwordElementName + '"]';
     if($(document).find(passwordElementName).length > 0){
       saveDialog.message = "以前登録したアカウントでloginしますか？";
       saveDialog.button = '<input type="submit" class="cushy_ext_login-js" style="width: 70px; height: 25px; margin: 0;" value="ログインする">';
       saveDialog.insert();
-
       $('input.cushy_ext_login-js').click(function(){
         $(document).find(passwordElementName).val(msg.accountData.password);
         $(document).find(loginElementName).val(msg.accountData.loginId);
+        $(document).find(loginElementName).closest('form').find('input[type="submit"]').click();
       });
     }
   }
